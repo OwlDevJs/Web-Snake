580e0bde5ef25641f8dcddaaccb552528567987e
OwlDevJs <owldev500@gmail.com>
2024-06-16 19:25:12 +0100

    Added snake and apple logic

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-const environement = document.getElementById("environement");
-
 const apple = document.getElementById("apple");
 
-const snake_part_1 = document.getElementById("snake-part-1");
+const snake = document.getElementById("snake");
 
-apple.style.display = "none";
+let snakeDirection = { x: 0, y: 0 };
 
-function addSnakePart() {
-  environement.innerHTML += `<div class="snake-part-2" id="snake-part-2"></div>`;
-  console.log("added");
-}
-
-function setApplePosition() {
-  if (apple.style.display === "none") {
-    apple.style.display = "block";
-  }
-
-  let apple_positionX = Math.floor(Math.random() * 100);
-
-  let apple_positionY = Math.floor(Math.random() * 100);
-
-  apple.style.right = `${apple_positionX}%`;
-
-  apple.style.top = `${apple_positionY}%`;
-}
-
-function MoveSnakeDown() {
-  snake_part_1.style.transform = "translate(0px,2000px)";
-  snake_part_1.style.transition = "all 10s ease-in-out";
-}
-
-function MoveSnakeRight() {
-  snake_part_1.style.transform = "translate(2000px, 0px)";
-  snake_part_1.style.transition = "all 10s ease-in-out";
-}
+let head = { top: 240, left: 490 };
 
 addEventListener("keydown", (event) => {
-  if (event.key === "ArrowDown") {
-    MoveSnakeDown();
-    //addSnakePart();
-    setApplePosition();
+  if (event.key === "ArrowUp") {
+    snakeDirection = { x: 0, y: -10 };
+  } else if (event.key === "ArrowDown") {
+    snakeDirection = { x: 0, y: 10 };
   } else if (event.key === "ArrowRight") {
-    MoveSnakeRight();
+    snakeDirection = { x: 10, y: 0 };
+  } else if (event.key === "ArrowLeft") {
+    snakeDirection = { x: -10, y: 0 };
+  } else {
+    return;
   }
 });
+
+addEventListener("load", () => {
+  PlaceApple();
+});
+
+setInterval(() => {
+  CheckGameOver();
+  MoveSnake();
+  CheckScore();
+}, 100);
+
+MoveSnake();
+
+function MoveSnake() {
+  head.top += snakeDirection.y;
+
+  head.left += snakeDirection.x;
+
+  snake.style.top = `${head.top}px`;
+
+  snake.style.left = `${head.left}px`;
+}
+
+function PlaceApple() {
+  let ApplePosition = {
+    x: Math.floor(Math.random() * 980),
+    y: Math.floor(Math.random() * 480),
+  };
+
+  apple.style.top = `${ApplePosition.y}px`;
+  apple.style.left = `${ApplePosition.x}px`;
+}
+
+function CheckScore() {
+  if (CheckOverlap(snake, apple)) {
+    PlaceApple();
+  }
+}
+
+function CheckOverlap(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+
+  return !(
+    rect2.left > rect1.right ||
+    rect2.right < rect1.left ||
+    rect2.top > rect1.bottom ||
+    rect2.bottom < rect1.top
+  );
+}
+
+function CheckGameOver() {
+  let snakePositionX = parseInt(snake.style.left.split("px")[0]);
+  let snakePositionY = parseInt(snake.style.top.split("px")[0]);
+
+  if (snakePositionX >= 980 || snakePositionY >= 480) {
+    console.log("Game Over");
+  }
+}
